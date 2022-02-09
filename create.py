8828c32f9b2169ff8a5b55056d092e7bf6319190
Avinash Ronanki <avinashronanki@carbmee.com>
2022-02-09 11:24:26 +0100

    new db

--- create.py
+++ create.py
@@ -11,11 +11,16 @@ c = conn.cursor()
 
 # Create table
 
-c.execute("""CREATE TABLE addresses (
-		first_name text,
-		last_name text,
-		address text,
-		city text,
-		state text,
-		zipcode integer
-		)""")
+# c.execute("""CREATE TABLE addresses (
+# 		first_name text,
+# 		last_name text,
+# 		address text,
+# 		city text,
+# 		state text,
+# 		zipcode integer
+# 		)""")
+# conn.commit()		
+
+c.execute(""" insert into addresses values ('john123','john123','john134','john','john','1234')
+		""")
+conn.commit()			
