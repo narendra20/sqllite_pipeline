4ca9109f1c3f309781d0e13c451d13adb90f6239
Avinash Ronanki <avinashronanki@carbmee.com>
2022-02-09 12:28:18 +0100

    add test

---
+++ test.py
@@ -0,0 +1,11 @@
+from gdrive import Gdrive
+
+gdrive = Gdrive('/Users/avinashronanki/secrets/')
+
+#gdrive.find_spreadsheet("DMG Mori manually matched input dataset")
+
+#gdrive.add_sheet("1RbfiLiwI3wQ_O6h99veBqclh-HJm7I6mxI-3e6JdaHw", "test api sheet")
+#gdrive.append_values("1RbfiLiwI3wQ_O6h99veBqclh-HJm7I6mxI-3e6JdaHw", "test api sheet", [[1, 2], [3, 4],[4, 5]])
+#df_list = df.values.tolist()
+# gdrive.get_values("1sGxKUdMMMWLkcmPfBJ-5NY3zX_HFsmRZqXc9vgAXBJE", "test api sheet")
+#gdrive.get_values_df("1RbfiLiwI3wQ_O6h99veBqclh-HJm7I6mxI-3e6JdaHw", "test api sheet")
